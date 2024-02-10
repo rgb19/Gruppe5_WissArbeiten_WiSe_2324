4a0af785b7aacfa119cfc1d27a68df7643346d7b
Garyzan <timo.meegdenburg@gmail.com>
2024-02-10 17:14:50 +0100

    extract address from name and estimate missing

--- read_dataset.R
+++ read_dataset.R
@@ -4,3 +4,14 @@ titanic <- read.csv("titanic.csv")
 
 # drop unneccessary "Ticket" and "PassengerId" columns
 titanic <- subset(titanic, select = -c(Ticket, PassengerId))
+
+# separate names into relevant particles and remove name
+titanic <- separate(titanic, "Name", into = c(NA, "form_of_address"), sep = "[,.] ", extra = "drop")
+
+# estimate missing age values by taking the mean of all other passengers with the the same form of address
+nas <- titanic[is.na(titanic$Age), ]
+for (foa in unique(nas$form_of_address)) {
+    subframe <- titanic[titanic$form_of_address == foa, ]
+    avg_age <- round(mean(subframe$Age, na.rm = TRUE))
+    titanic[is.na(titanic$Age) & titanic$form_of_address == foa, ]$Age <- avg_age
+}
