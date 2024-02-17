d6a396be4573d3b5ddb6c8bd84413d493b2b2525
Garyzan <timo.meegdenburg@gmail.com>
2024-02-17 17:59:57 +0100

    add empirical entropy helper function

--- Funktionen-R-Skript 2.R
+++ Funktionen-R-Skript 2.R
@@ -1,1 +1,9 @@
 # Helfer-Funktionen
+
+empirical_entropy <- function(data) {
+    empirical_frequencies <- sapply(unique(data), function(x) {
+        h <- length(data[data == x]) / length(data)
+        return(h * log(h))
+    })
+    return(-sum(empirical_frequencies) / log(length(empirical_frequencies)))
+}
