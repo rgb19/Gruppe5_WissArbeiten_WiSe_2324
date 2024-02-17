62f9531736609086ab47fa6b39fd0a1461a93a33
Garyzan <timo.meegdenburg@gmail.com>
2024-02-17 18:00:11 +0100

    add median and empirical entropy

--- Funktionen-R-Skript 1.R
+++ Funktionen-R-Skript 1.R
@@ -1,13 +1,20 @@
 # Hauptfunktionen:
-
+source("Funktionen-R-Skript 2.R")
 
 categorial_stats <- function(df, column_names, ordered = FALSE) {
     stopifnot(length(column_names) == length(ordered))
 
     outlist <- list()
 
-    for (colname in column_names) {
+    for (i in 1:length(column_names)) {
         sublist <- list()
+        data <- df[[column_names[i]]]
+
+        if (ordered[i]) {
+            sublist[["median"]] <- median(data)
+        }
+
+        sublist[["Empirical Entropy"]] <- empirical_entropy(data)
 
         outlist[["colname"]] <- sublist
     }
