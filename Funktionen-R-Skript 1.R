99ed5d79d44f1a36b736d50b34df963d8e898cc3
Garyzan <timo.meegdenburg@gmail.com>
2024-02-17 17:16:33 +0100

    make basic code outline

--- Funktionen-R-Skript 1.R
+++ Funktionen-R-Skript 1.R
@@ -1,1 +1,16 @@
 # Hauptfunktionen:
+
+
+categorial_stats <- function(df, column_names, ordered = FALSE) {
+    stopifnot(length(column_names) == length(ordered))
+
+    outlist <- list()
+
+    for (colname in column_names) {
+        sublist <- list()
+
+        outlist[["colname"]] <- sublist
+    }
+
+    return(outlist)
+}
