4eadbd99a874b7d5509a177b0dc27c025822fbaf
Garyzan <timo.meegdenburg@gmail.com>
2024-02-18 15:11:35 +0100

    added more quantiles + mode

--- Funktionen-R-Skript 1.R
+++ Funktionen-R-Skript 1.R
@@ -11,14 +11,24 @@ categorial_stats <- function(df, column_names, ordered = FALSE) {
         data <- df[[column_names[i]]]
 
         if (ordered[i]) {
+            data_sorted <- sort(data)
+            indices <- floor(c(0.25, 0.75) * length(data))
+            sublist[["0.25 quantile"]] <- data_sorted[indices[1]]
             sublist[["median"]] <- median(data)
+            sublist[["0.75 quantile"]] <- data_sorted[indices[2]]
         }
 
-        sublist[["Empirical entropy"]] <- empirical_entropy(data)
-        sublist[["Phi dispersion"]] <- phi_dispersion(data)
+        h_k <- sapply(unique(data), function(x) length(data[data == x]) / length(data))
 
-        outlist[["colname"]] <- sublist
+        sublist[["Mode"]] <- unique(data)[h_k == max(h_k)]
+        sublist[["Empirical entropy"]] <- empirical_entropy(h_k)
+        sublist[["Phi dispersion"]] <- phi_dispersion(h_k)
+
+        outlist[[column_names[i]]] <- sublist
     }
 
     return(outlist)
 }
+
+titanic <- read.csv("titanic.csv")
+print(categorial_stats(titanic, "Pclass", TRUE))
--- Funktionen-R-Skript 2.R
+++ Funktionen-R-Skript 2.R
@@ -1,15 +1,11 @@
 # Helfer-Funktionen
 
-empirical_entropy <- function(data) {
-    empirical_frequencies <- sapply(unique(data), function(x) {
-        h <- length(data[data == x]) / length(data)
-        return(h * log(h))
-    })
+empirical_entropy <- function(h_k) {
+    empirical_frequencies <- h_k * log(h_k)
     return(-sum(empirical_frequencies) / log(length(empirical_frequencies)))
 }
 
-phi_dispersion <- function(data) {
-    h_k <- sapply(unique(data), function(x) length(data[data == x]) / length(data))
+phi_dispersion <- function(h_k) {
     phi_min <- 2 * (1 - max(h_k))
     phi_max <- sum(abs(h_k - 1 / length(h_k)))
     return(phi_min / (phi_min + phi_max))
