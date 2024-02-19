a6e5b4d5ae5ff0640c03bb7670a52b4f58412aaf
Garyzan <timo.meegdenburg@gmail.com>
2024-02-19 12:53:03 +0100

    added documentation

--- Funktionen-R-Skript 2.R
+++ Funktionen-R-Skript 2.R
@@ -1,10 +1,22 @@
 # Helfer-Funktionen
 
+# empirical_entropy - Berechnet die empirische Entropie aus einem Vektor
+#                     relativer Häufigkeiten
+#
+# Input: h_k - numerischer Vektor der relativen Häufigkeiten
+#
+# Output: empirische Entropie
 empirical_entropy <- function(h_k) {
     empirical_frequencies <- h_k * log(h_k)
     return(-sum(empirical_frequencies) / log(length(empirical_frequencies)))
 }
 
+# phi_dispersion - Berechnet das Phi-Streuungsmaß aus einem Vektor relativer
+#                  Häufigkeiten
+#
+# Input: h_k - numerischer Vektor der relativen Häufigkeiten
+#
+# Output: Phi-Streuungsmaß
 phi_dispersion <- function(h_k) {
     phi_min <- 2 * (1 - max(h_k))
     phi_max <- sum(abs(h_k - 1 / length(h_k)))
