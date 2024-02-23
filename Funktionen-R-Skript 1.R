dacadb46a6ca343436536ec79f4b4d224e396c05
Oumaima <36044633+oumaimamaatallah@users.noreply.github.com>
2024-02-23 21:39:59 +0100

    Update Funktionen-R-Skript 1.R

--- Funktionen-R-Skript 1.R
+++ Funktionen-R-Skript 1.R
@@ -70,12 +70,7 @@ categorial_stats <- function(df, column_names, ordered = FALSE) {
     return(outlist)
 }
 
-# 2.iii. 3)
-# bivariate_stats -                
-# Input:
-# Output: Benannte Liste. Jedes Objekt der Liste hat den Namen der analysierten
-#         Spalte und ist wiederum eine benannte Liste                    
-
+              
 #2.iv)
 # Berechnet Durchschnitt und Varianz von einer metrischen Variable im 
 #   Zusammenhang einer Dichotomen Variable und führt einen T-Test mit diesen 
